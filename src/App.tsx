4aca8c057c97a34214df6e89bf09f741f04688ba
infantGrandpa <babraham341@gmail.com>
2023-08-12 13:23:04 -0400

    Build Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Container, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -6,19 +6,35 @@ function App() {
         <Grid
             templateAreas={{
                 base: `"nav" "main"`,
-                lg: `"nav nav" "aside main"`,
+                md: `"nav nav" "aside main"`,
             }}
         >
             <GridItem area="nav">
                 <NavBar></NavBar>
             </GridItem>
-            <Show above="lg">
-                <GridItem area="aside" bg="gold">
-                    Aside
+            <Show above="md">
+                <GridItem area="aside">
+                    <Container
+                        border="1px"
+                        borderColor="gray.200"
+                        borderRadius="3px"
+                        m="5px"
+                        p="5px"
+                    >
+                        Aside
+                    </Container>
                 </GridItem>
             </Show>
-            <GridItem area="main" bg="dodgerblue">
-                Main
+            <GridItem area="main">
+                <Container
+                    border="1px"
+                    borderColor="gray.200"
+                    borderRadius="3px"
+                    m="5px"
+                    p="5px"
+                >
+                    Main
+                </Container>
             </GridItem>
         </Grid>
     );
