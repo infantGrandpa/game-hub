34a60443fcf30dd3c9584247bc326245f0cc0056
infantGrandpa <babraham341@gmail.com>
2023-08-14 11:30:56 -0400

    Show Game Card Skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -16,7 +16,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius={"4"} overflow={"hidden"}>
+        <Card width="300px" borderRadius={"4"} overflow={"hidden"}>
             <AspectRatio ratio={16 / 9}>
                 <Image src={getCroppedImageUrl(game.background_image)} />
             </AspectRatio>
