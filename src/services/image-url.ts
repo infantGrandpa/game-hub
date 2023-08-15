e595efd384b597e4f569bc022aba77884f0109fa
infantGrandpa <babraham341@gmail.com>
2023-08-15 18:18:46 -0400

    Sort Games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+    if (!url) return "";
     const targetString = "media/";
     const index = url.indexOf(targetString) + targetString.length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
